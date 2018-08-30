cbc710fecc9d63a4f980443893f50b8a05cc4a97
yelena guliyeva <gulbadam@gulbadam.com>
2018-08-29 17:53:36 -0700

    header

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 /** @format */
+import React from "react";
+import {
+    Text,
+    AppRegistry,
+    View,
+    WebView,
+    StyleSheet
+} from 'react-native';
+import Header from "./src/components/header";
+const App = () =>  (
+<Header headerText={'Albums'}/>
+);
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+AppRegistry.registerComponent('albums', () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+        <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+const styles = {
+    viewStyle: {
+    backgroundColor: '#ff7788',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    paddingBottom: 15,
+    // shadowColor: '#000',
+    // shadowOffset: { width: 5, height: 20 },
+    // shadowOpacity: 0.9,
+    elevation: 5,
+    position: 'relative',
+    borderBottomColor: 'blue',
+    borderBottomEndRadius: 40,
+    borderBottomStartRadius: 40,
+
+    
+},
+    textStyle: {
+        fontSize: 30
+    }
+}
+export default Header;
