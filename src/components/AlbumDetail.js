31041df8e252986a6a5c63e4978cdef100f7c90e
yelena guliyeva <gulbadam@gulbadam.com>
2018-08-30 17:49:25 -0700

    scroll

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const {
+        title,
+        thumbnail_image,
+        artist, image
+    } =  album; 
+    const {
+        headerTextStyle,
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle, imageStyle
+    } = styles;
 return (
     <Card>
     <CardSection>
-    <Text>{props.album.title}</Text>
-    </CardSection>
+    <View style={thumbnailContainerStyle}>
+    <Image style={thumbnailStyle} source = {{ uri: thumbnail_image }} />
+    </View>
+    <View style={headerContentStyle}>
+    <Text style = {headerTextStyle} >{title}</Text>
+    <Text>{artist}</Text>
+    </View>
+    </CardSection> 
+    <CardSection>
+    <Image style={imageStyle} source = {{ uri: image }} />
+    </CardSection> 
     </Card>
 );
 };
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+       fontSize: 20,
+       fontWeight: 'bold'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    imageStyle: {
+       height: 300,
+       flex: 1,
+       width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        margin: 3,
+        marginRight: 8
+    }
+}
 export default AlbumDetail;
